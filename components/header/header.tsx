990bcf17da3622c67b64145e5a5d2b0fdf713c32
David Snyder <davidgraygs4@gmail.com>
2021-04-11 01:24:10 -0400

    add Anchor component

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,12 +1,19 @@
 const Header = () => {
-
-
-    return <header>
-        <div>
-            <div></div>
-            <nav></nav>
-        </div>
+  return (
+    <header>
+      <div>
+        <div></div>
+        <nav>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </nav>
+      </div>
     </header>
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ components/ui/Anchor.tsx
@@ -0,0 +1,52 @@
+import {
+  AnchorHTMLAttributes,
+  DetailedHTMLProps,
+  forwardRef,
+  LegacyRef,
+} from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+interface AnchorProps
+  extends DetailedHTMLProps<
+    AnchorHTMLAttributes<HTMLAnchorElement>,
+    HTMLAnchorElement
+  > {
+  newTab?: boolean;
+  activeClassName?: string;
+  local?: boolean;
+  as?: string;
+}
+
+const Anchor = forwardRef<HTMLAnchorElement, AnchorProps>(
+  ({ newTab, ...props }: AnchorProps, ref) => {
+    const Rel = newTab ? "noopener noreferrer" : props.rel;
+    const Target = newTab ? "_blank" : null;
+
+    const { asPath } = useRouter();
+
+    const active = props.href === asPath;
+
+    const component = (
+      <a
+        ref={ref}
+        {...props}
+        className={`${props.className} ${
+          active ? props.activeClassName : ""
+        }`.trim()}
+        rel={Rel}
+        target={Target}
+      ></a>
+    );
+
+    return props.local ? (
+      <Link href={props.href} as={props.as}>
+        {component}
+      </Link>
+    ) : (
+      component
+    );
+  }
+);
+
+export default Anchor;
